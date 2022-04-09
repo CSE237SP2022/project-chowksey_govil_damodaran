264a77af790eed0e8c70cffedad79de8e57b1687
Ram Damodaran <dramvignesh@gmail.com>
2022-04-09 11:18:47 -0500

    created main method for recommendation class

---
+++ src/recommendation/Recommendation.java
@@ -0,0 +1,24 @@
+package recommendation;
+
+import java.util.Scanner;
+
+import carbonFootprintCalculator.CarbonFootPrintCalculator;
+
+public class Recommendation {
+	
+	Scanner reader;
+	
+	public Recommendation() {
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to our recommendation section. Please choose one of the following:");
+		System.out.println("(1) - Recommendations based on your carbon footprint. Note: Please have completed the Carbon Footprint Calculator first");
+		System.out.println("(2) - Take some pledges to reduce your carbon footprint");
+		
+		
+		
+	}
+
+}
