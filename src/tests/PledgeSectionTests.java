a5bae748123ff06eebc1151cc0b15809eca97fdb
dhruvgovil <46689419+dhruvgovil@users.noreply.github.com>
2022-04-25 22:33:43 -0500

    Added pledge section unit tests

---
+++ src/tests/PledgeSectionTests.java
@@ -0,0 +1,25 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+
+import org.junit.jupiter.api.Test;
+
+import classes.CarbonFootprintPledgeSection;
+
+class PledgeSectionTests {
+	
+	//test only public class method per instructor suggestion
+	@Test
+	void testMainPledgeMethod() {
+		StringBuilder sb = new StringBuilder();
+	    sb.append("10.0 1 1 1 10.0 1 1 1");
+	    String data = sb.toString();
+	    System.setIn(new ByteArrayInputStream(data.getBytes()));
+	    CarbonFootprintPledgeSection cps = new CarbonFootprintPledgeSection();
+	    String result = cps.mainPledgeMethod();
+	    assertEquals("Great!",result);
+	}
+
+}
